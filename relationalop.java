634b5bf905d498713d6fc739ffc563ee6871c8e1
priti <pritigawade2309@gmail.com>
2024-10-15 14:10:20 +0530

    Add files via upload

---
+++ relationalop.java
@@ -0,0 +1,22 @@
+package operators;
+
+public class relationalop {
+
+	public static void main(String[] args) {
+		// (==,<=,>=,!=)
+				int x=10;
+				int y=20;
+				boolean res1=x>=y;
+		System.out.println(res1);
+		
+		boolean res2=x<=y;
+		System.out.println(res2);
+		
+		boolean res3=x==y;
+		System.out.println(res3);
+		
+		boolean res4=x!=y;
+		System.out.println(res4);
+	}
+
+}
