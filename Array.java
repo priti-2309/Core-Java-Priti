062ed5967fe675397eefc9c91bfaa578e82ce795
priti <pritigawade2309@gmail.com>
2024-10-17 13:23:18 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class Array {
+
+	public static void main(String[] args) {
+		String animals[]= {"Dog","Cat","Elephant","Lion","Tiger","Cow","Bull","Sheep","Squirrel","Rabbit"};
+		
+		for(int i=0;i<10;i++)
+		{
+			System.out.println(animals[i]);
+		}
+			
+	}
+
+}
